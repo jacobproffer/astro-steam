e0a754b21c2d1e895c3f42ebea2a3c6e07abba69
jacobproffer <jacobhardingproffer@gmail.com>
2025-12-28 17:14:25 -0500

    feat: add Backgrounds component and chooseBackground utility; update RecentGames styles

---
+++ src/utils/chooseBackground.js
@@ -0,0 +1,19 @@
+import armaBackground from '@assets/img/arma-background.jpg';
+import left4Dead2Background from '@assets/img/left-4-dead-2-background.jpg';
+import runescapeBackground from '@assets/img/runescape-background.jpg';
+import defaultBackground from '@assets/img/default-background.jpg';
+
+const chooseBackground = (game) => {
+    switch (game) {
+        case 'Arma 3':
+            return armaBackground;
+        case 'Left 4 Dead 2':
+            return left4Dead2Background;
+        case 'Runescape':
+            return runescapeBackground;
+        default:
+            return defaultBackground;
+    }
+};
+
+export default chooseBackground;
