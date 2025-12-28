ed17e26de6d48ab960164ca44f6c720207af4a3b
jacobproffer <jacobhardingproffer@gmail.com>
2025-12-27 21:18:33 -0500

    feat: integrate Steam API to fetch user profile and recent games

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.example.com",
+  output: 'server',
+  adapter: netlify(),
   integrations: [sitemap()],
   vite: { plugins: [tailwindcss()], },
 });
