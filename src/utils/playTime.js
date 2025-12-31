06df201e0ee6956d66ea52e6cbff5ef89a8927a0
jacobproffer <jacobhardingproffer@gmail.com>
2025-12-31 10:03:12 -0500

    fix: improve playtime formatting and ensure total playtime is displayed for games

--- src/utils/playTime.js
+++ src/utils/playTime.js
@@ -2,7 +2,8 @@ const playTime = (minutes) => {
     let timeOutput = '';
 
     if (minutes >= 120) {
-        timeOutput = `${Math.round(minutes / 60)} hours`;
+        const hours = Math.round(minutes / 60);
+        timeOutput = `${hours.toLocaleString()} hours`;
     } else if (minutes > 60 && minutes < 120) {
         timeOutput = '1 hour';
     } else if (minutes > 1 && minutes < 60) {
